284fbb11b9200689bf952112aab9716bad1fccb0
François Ingelrest <Francois.Ingelrest@gmail.com>
2010-07-28 21:26:54 +0200

    * Simplified code

--- src/modules/Explorer.py
+++ src/modules/Explorer.py
@@ -21,20 +21,17 @@ import gobject, gtk, modules
 from tools   import consts, icons, prefs
 from gettext import gettext as _
 
-MOD_INFO = ('Explorer', 'Explorer', '', [], True, False)
+MOD_INFO              = ('Explorer', 'Explorer', '', [], True, False)
+DEFAULT_LAST_EXPLORER = ('', '')     # Module name and explorer name
 
 # The rows in the combo box
 (
-    ROW_PIXBUF,
-    ROW_NAME,
-    ROW_MODULE,
-    ROW_IS_SEPARATOR,
-    ROW_IS_HEADER
+    ROW_PIXBUF,         # Icon displayed in front of the entry
+    ROW_NAME,           # Name of the entry
+    ROW_MODULE,         # Name of the module the entry is associated to
+    ROW_PAGE_NUM,       # Number of the notebook page the entry is associated to
+    ROW_IS_HEADER,      # True if the entry is an header (used to separate different modules)
 ) = range(5)
-
-
-# Default preferences
-DEFAULT_LAST_EXPLORER = ('', '')     # Module name and explorer name
 
 
 class Explorer(modules.Module):
@@ -49,25 +46,22 @@ class Explorer(modules.Module):
                                       })
 
         # Attributes
-        self.combo        = prefs.getWidgetsTree().get_widget('combo-explorer')
-        self.widgets      = {}
-        self.notebook     = prefs.getWidgetsTree().get_widget('notebook-explorer')
-        self.explorers    = {}
-        self.currExplorer = None
+        self.store           = gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_BOOLEAN)
+        self.combo           = prefs.getWidgetsTree().get_widget('combo-explorer')
+        txtRenderer          = gtk.CellRendererText()
+        pixRenderer          = gtk.CellRendererPixbuf()
+        self.notebook        = prefs.getWidgetsTree().get_widget('notebook-explorer')
+        self.allExplorers    = {}
+        self.notebookPages   = {}
+        self.currExplorerIdx = 0
         # Setup the combo box
-        txtRenderer = gtk.CellRendererText()
-        pixRenderer = gtk.CellRendererPixbuf()
-
+        txtRenderer.set_property('xpad', 6)
         self.combo.pack_start(pixRenderer, False)
         self.combo.add_attribute(pixRenderer, 'pixbuf', ROW_PIXBUF)
         self.combo.pack_start(txtRenderer, True)
         self.combo.add_attribute(txtRenderer, 'markup', ROW_NAME)
         self.combo.set_sensitive(False)
-        self.combo.set_row_separator_func(lambda model, iter: model.get_value(iter, ROW_IS_SEPARATOR))
         self.combo.set_cell_data_func(txtRenderer, self.__cellDataFunction)
-        txtRenderer.set_property('xpad', 6)
-        # Setup the liststore
-        self.store = gtk.ListStore(gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_BOOLEAN, gobject.TYPE_BOOLEAN)
         self.combo.set_model(self.store)
         # Setup the notebook
         label = gtk.Label(_('Please select an explorer\nin the combo box below.'))
@@ -83,28 +77,33 @@ class Explorer(modules.Module):
         else:                                    cellRenderer.set_property('xalign', 0.0)
 
 
-    def __fillComboBox(self):
+    def fillComboBox(self):
         """ Fill the combo box """
         idx = self.combo.get_active()
 
         if idx == -1: (selectedModule, selectedExplorer) = prefs.get(__name__, 'last-explorer', DEFAULT_LAST_EXPLORER)
         else:         (selectedModule, selectedExplorer) = self.store.get(self.store.get_iter(idx), ROW_MODULE, ROW_NAME)
 
-        restoredIdx = None
         self.combo.freeze_child_notify()
         self.store.clear()
 
-        for module in sorted(self.explorers):
-            self.store.append((None, '<b>%s</b>' % module, '', False, True))
-            for (explorer, (pixbuf, widget)) in sorted(self.explorers[module].iteritems()):
-                self.store.append((pixbuf, explorer, module, False, False))
+        restoredIdx    = None
+        previousModule = None
 
-                if module == selectedModule and explorer == selectedExplorer:
-                    restoredIdx = len(self.store) - 1
+        for ((module, explorer), (pixbuf, widget)) in sorted(self.allExplorers.iteritems()):
+
+            if module != previousModule:
+                self.store.append((None, '<b>%s</b>' % module, '', -1, True))
+                previousModule = module
+
+            self.store.append((pixbuf, explorer, module, self.notebookPages[widget], False))
+
+            if module == selectedModule and explorer == selectedExplorer:
+                restoredIdx = len(self.store) - 1
 
         if restoredIdx is None:
+            self.currExplorerIdx = 0
             self.notebook.set_current_page(0)
-            self.currExplorer = -1
         else:
             self.combo.set_active(restoredIdx)
 
@@ -117,66 +116,38 @@ class Explorer(modules.Module):
 
     def onAddExplorer(self, modName, expName, icon, widget):
         """ Add a new explorer to the combo box """
-        if icon is None:
-            icon = icons.dirMenuIcon()
+        if icon is None: self.allExplorers[(modName, expName)] = (icons.dirMenuIcon(), widget)
+        else:            self.allExplorers[(modName, expName)] = (icon, widget)
 
-        if modName not in self.explorers: self.explorers[modName]          = {expName: (icon, widget)}
-        else:                             self.explorers[modName][expName] = (icon, widget)
-
-        if widget not in self.widgets:
-            self.widgets[widget] = self.notebook.get_n_pages()
+        if widget not in self.notebookPages:
+            self.notebookPages[widget] = self.notebook.get_n_pages()
             self.notebook.append_page(widget)
 
-        self.__fillComboBox()
+        self.fillComboBox()
 
 
     def onRemoveExplorer(self, modName, expName):
         """ Remove an existing explorer from the combo box """
-        (pixbuf, widget) = self.explorers[modName][expName]
-        del self.explorers[modName][expName]
-
-        delWidget = True
-        for (expName, (pixbuf, widget2)) in self.explorers[modName].iteritems():
-            if widget2 is widget:
-                delWidget = False
-                break
-
-        if delWidget:
-            numPage = self.widgets[widget]
-            del self.widgets[widget]
-            self.notebook.remove_page(numPage)
-            for (widget, numPage2) in self.widgets.iteritems():
-                if numPage2 > numPage:
-                    self.widgets[widget] = numPage2 - 1
-
-        self.__fillComboBox()
+        del self.allExplorers[(modName, expName)]
+        self.fillComboBox()
 
 
     def onRenameExplorer(self, modName, expName, newExpName):
         """ Rename the given explorer """
-        # Beware of this special case: This screws the dictionnary
-        if newExpName == expName:
-            return
+        if newExpName != expName:
+            self.allExplorers[(modName, newExpName)] = self.allExplorers[(modName, expName)]
+            del self.allExplorers[(modName, expName)]
 
-        # Modify the dictionnary
-        self.explorers[modName][newExpName] = self.explorers[modName][expName]
-        del self.explorers[modName][expName]
+            # If the explorer we're renaming is currently selected, we need to rename the row
+            # Otherwise, fillComboBox() won't be able to keep it selected
+            idx = self.combo.get_active()
+            if idx != -1:
+                (selModName, selExpName) = self.store.get(self.store.get_iter(idx), ROW_MODULE, ROW_NAME)
+                if selModName == modName and selExpName == expName:
+                    self.store.set(self.store.get_iter(idx), ROW_NAME, newExpName)
+                    prefs.set(__name__, 'last-explorer', (modName, newExpName))
 
-        # If the explorer we're renaming is currently selected, we need to rename the row
-        # Otherwise, __fillComboBox() won't be able to keep it selected
-        idx = self.combo.get_active()
-        if idx != -1:
-            (selModName, selExpName) = self.store.get(self.store.get_iter(idx), ROW_MODULE, ROW_NAME)
-            if selModName == modName and selExpName == expName:
-                self.store.set(self.store.get_iter(idx), ROW_NAME, newExpName)
-
-        # This is needed to sort the explorers according to the new name
-        self.__fillComboBox()
-
-        # Changed the saved name if needed
-        (savedModName, savedExpName) = prefs.get(__name__, 'last-explorer', (modName, expName))
-        if savedModName == modName and savedExpName == expName:
-            prefs.set(__name__, 'last-explorer', (modName, newExpName))
+            self.fillComboBox()
 
 
     # --== GTK handlers ==--
@@ -184,21 +155,18 @@ class Explorer(modules.Module):
 
     def onChanged(self, combo):
         """ A new explorer has been selected with the combo box """
-        currExplorer = combo.get_active()
+        idx = combo.get_active()
 
-        if currExplorer == -1:
+        if idx == -1:
             self.notebook.set_current_page(0)
+            return
+
+        pixbuf, explorer, module, page, isHeader = self.store[idx]
+
+        if isHeader:
+            combo.set_active(self.currExplorerIdx)
         else:
-            modName, expName, isHeader = self.store.get(self.store.get_iter(currExplorer), ROW_MODULE, ROW_NAME, ROW_IS_HEADER)
-
-            if isHeader:
-                if self.currExplorer is not None:
-                    combo.set_active(self.currExplorer)
-            else:
-                if self.currExplorer != currExplorer:
-                    self.currExplorer = currExplorer
-                    prefs.set(__name__, 'last-explorer', (modName, expName))
-                    modules.postMsg(consts.MSG_EVT_EXPLORER_CHANGED, {'modName': modName, 'expName': expName})
-
-                (pixbuf, widget) = self.explorers[modName][expName]
-                self.notebook.set_current_page(self.widgets[widget])
+            self.currExplorerIdx = idx
+            prefs.set(__name__, 'last-explorer', (module, explorer))
+            modules.postMsg(consts.MSG_EVT_EXPLORER_CHANGED, {'modName': module, 'expName': explorer})
+            self.notebook.set_current_page(page)
