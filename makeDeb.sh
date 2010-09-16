feda2c0207fe8be17208c0aee0a9ca316ca36447
Jendrik Seipp <jendrikseipp@web.de>
2010-09-16 21:06:42 +0200

    rebranding in man file and deb creation script

--- makeDeb.sh
+++ makeDeb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-INSTALL_DIR="/usr/share/decibel-audio-player"
+INSTALL_DIR="/usr/share/pogo"
 MAN_DIR="/usr/share/man/man1"
 BIN_DIR="/usr/bin"
 APP_DIR="/usr/share/applications"
@@ -12,40 +12,40 @@ VERSION=`cat control-install | grep "Version" | cut -d\  -f2`
 find -type f -name "*.pyc" -exec rm -f {} \;
 find -type f -name "*.pyo" -exec rm -f {} \;
 
-mkdir -p decibel-audio-player/DEBIAN
-cp control-install decibel-audio-player/DEBIAN/control
+mkdir -p pogo/DEBIAN
+cp control-install pogo/DEBIAN/control
 
-mkdir -p "decibel-audio-player"$ICO_DIR
-cp pix/decibel-audio-player.png "decibel-audio-player"$ICO_DIR
+mkdir -p "pogo"$ICO_DIR
+cp pix/pogo.png "pogo"$ICO_DIR
 
-mkdir -p "decibel-audio-player"$MAN_DIR
-cp doc/decibel-audio-player.1 "decibel-audio-player"$MAN_DIR
-cp doc/decibel-audio-player-remote.1 "decibel-audio-player"$MAN_DIR
+mkdir -p "pogo"$MAN_DIR
+cp doc/pogo.1 "pogo"$MAN_DIR
+cp doc/pogo-remote.1 "pogo"$MAN_DIR
 
-mkdir -p "decibel-audio-player"$INSTALL_DIR
+mkdir -p "pogo"$INSTALL_DIR
 
-cp -R src "decibel-audio-player"$INSTALL_DIR"/"
+cp -R src "pogo"$INSTALL_DIR"/"
 
 # Resources
-mkdir -p "decibel-audio-player"$INSTALL_DIR"/res"
+mkdir -p "pogo"$INSTALL_DIR"/res"
 cd res
 ./optiglade.py
 cd ..
-cp res/*.glade "decibel-audio-player"$INSTALL_DIR"/res/"
+cp res/*.glade "pogo"$INSTALL_DIR"/res/"
 
-mkdir -p "decibel-audio-player"$INSTALL_DIR"/pix"
-cp pix/*.png "decibel-audio-player"$INSTALL_DIR"/pix/"
+mkdir -p "pogo"$INSTALL_DIR"/pix"
+cp pix/*.png "pogo"$INSTALL_DIR"/pix/"
 
-mkdir -p "decibel-audio-player"$INSTALL_DIR"/doc"
-cp doc/LICENCE "decibel-audio-player"$INSTALL_DIR"/doc/"
-cp doc/ChangeLog "decibel-audio-player"$INSTALL_DIR"/doc/"
+mkdir -p "pogo"$INSTALL_DIR"/doc"
+cp doc/LICENCE "pogo"$INSTALL_DIR"/doc/"
+cp doc/ChangeLog "pogo"$INSTALL_DIR"/doc/"
 
-mkdir -p "decibel-audio-player"$APP_DIR
-cp res/*.desktop "decibel-audio-player"$APP_DIR
+mkdir -p "pogo"$APP_DIR
+cp res/*.desktop "pogo"$APP_DIR
 
-mkdir -p "decibel-audio-player"$BIN_DIR
-cp start-package.sh "decibel-audio-player"$BIN_DIR"/decibel-audio-player"
-cp start-remote-package.sh "decibel-audio-player"$BIN_DIR"/decibel-audio-player-remote"
+mkdir -p "pogo"$BIN_DIR
+cp start-package.sh "pogo"$BIN_DIR"/pogo"
+cp start-remote-package.sh "pogo"$BIN_DIR"/pogo-remote"
 
 # Create locales
 rm -rf locale
@@ -54,14 +54,14 @@ make dist
 cd ..
 
 # Copy locales
-mkdir -p "decibel-audio-player"$LOC_DIR
-cp -R locale/* "decibel-audio-player"$LOC_DIR
+mkdir -p "pogo"$LOC_DIR
+cp -R locale/* "pogo"$LOC_DIR
 
 # Remove .svn directories
-find "decibel-audio-player" -type d -name ".svn" -exec rm -rf {} \; > /dev/null 2>&1
+find "pogo" -type d -name ".svn" -exec rm -rf {} \; > /dev/null 2>&1
 
 # Sources: Make sure to remove non-Python files
-find "decibel-audio-player"$INSTALL_DIR"/src" -type f ! -name "*.py" -exec rm -f {} \;
+find "pogo"$INSTALL_DIR"/src" -type f ! -name "*.py" -exec rm -f {} \;
 
-dpkg-deb --build decibel-audio-player decibel-audio-player-$VERSION.deb
-rm -rf decibel-audio-player
+dpkg-deb --build pogo pogo-$VERSION.deb
+rm -rf pogo
