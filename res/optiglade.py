42abe24c379b449c3e8be0baf6fdcaec8729b0bb
Jendrik Seipp <jendrikseipp@web.de>
2010-09-26 12:55:05 +0200

    remove fileexplorer config code

--- res/optiglade.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-for gladeFile in [os.path.join('./', file) for file in os.listdir('./') if file.endswith('.glade')]:
-    os.system('vim %s -c "%%g/^<!--.*-->$/d" -c "%%g/^.*GDK_POINTER_MOTION_MASK.*$/d" -c "%%s/^\\s\\+//" -c "x"' % gladeFile)
