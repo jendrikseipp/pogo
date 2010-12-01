7d1fff6b6bc4f281a00b2159bac4fd1ee363b557
Jendrik Seipp <jendrikseipp@web.de>
2010-12-01 16:43:19 +0100

    make code more python2.5 compatible

--- src/modules/Search.py
+++ src/modules/Search.py
@@ -111,9 +111,9 @@ class Search(modules.ThreadedModule):
         
     def cache(self):
         ''' Cache results for a faster first search '''
-        for index, path in enumerate(self.paths, 1):
+        for index, path in enumerate(self.paths):
             # Cache dirs one by one after a small timeout 
-            gobject.timeout_add_seconds(5 * index, self.search_dir, path, \
+            gobject.timeout_add_seconds(5 * (index+1), self.search_dir, path, \
                                         'caching_files')
 
 
