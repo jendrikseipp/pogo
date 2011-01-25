c4e073c4df70e23cb09efdf303417a2db8fc4d67
Jendrik Seipp <jendrikseipp@web.de>
2011-01-25 22:34:55 +0100

    save cover size, fix compiz cover problem

--- src/modules/TrackPanel.py
+++ src/modules/TrackPanel.py
@@ -21,7 +21,7 @@ import logging
 
 import gobject, gtk, modules, os.path, tools
 
-from tools   import consts
+from tools   import consts, prefs
 from gettext import gettext as _
 
 MOD_INFO = ('Track Panel', 'Track Panel', '', [], True, False)
@@ -40,6 +40,7 @@ class TrackPanel(modules.Module):
                         consts.MSG_CMD_SET_COVER:   self.onSetCover,
                         consts.MSG_EVT_NEW_TRACK:   self.onNewTrack,
                         consts.MSG_EVT_APP_STARTED: self.onAppStarted,
+                        consts.MSG_EVT_APP_QUIT:    self.onAppQuit,
                    }
 
         modules.Module.__init__(self, handlers)
@@ -48,13 +49,13 @@ class TrackPanel(modules.Module):
     def __setTitle(self, title, length=None):
         """ Change the title of the current track """
         title = tools.htmlEscape(title)
-        
+
         ## Do not add length
         length = None
 
         if length is None: self.txtTitle.set_markup('<span size="larger"><b>%s</b></span>' % title)
         else:              self.txtTitle.set_markup('<span size="larger"><b>%s</b></span>  [%s]' % (title, tools.sec2str(length)))
-        
+
 
 
     # --== Message handlers ==--
@@ -66,10 +67,15 @@ class TrackPanel(modules.Module):
         wTree                  = tools.prefs.getWidgetsTree()
         ##self.txtTitle          = wTree.get_widget('lbl-trkTitle')
         self.currTrack         = None
-        
+
         ##
         #self.txtTitle.hide()
-        self.cover_spot = CoverSpot()
+        show_thumb = prefs.get(__name__, 'show_thumb', True)
+        self.cover_spot = CoverSpot(show_thumb)
+
+    def onAppQuit(self):
+        """ The application is about to terminate """
+        prefs.set(__name__, 'show_thumb', self.cover_spot.show_thumb)
 
 
     def onNewTrack(self, track):
@@ -97,71 +103,74 @@ class TrackPanel(modules.Module):
         # Must check if currTrack is not None, because '==' calls the cmp() method and this fails on None
         if self.currTrack is not None and track == self.currTrack:
             self.cover_spot.set_images(pathFullSize, pathThumbnail)
-            
-            
-            
+
+
+
 class CoverSpot(object):
-    def __init__(self):
-        self.show_thumb = None
+    def __init__(self, show_thumb):
+        self.show_thumb = show_thumb
         self.has_focus = True
-        
+
         self.cover_window = CoverWindow()
         self.thumb_window = CoverWindow()
-        
+
         # Switch sizes, when clicked
         self.cover_window.evtBox.connect('button-press-event', self.onCoverClicked, True)
         self.thumb_window.evtBox.connect('button-press-event', self.onCoverClicked, False)
-        
+
         wTree = tools.prefs.getWidgetsTree()
         main_win = wTree.get_widget('win-main')
-        
+
         main_win.connect('focus-out-event', self.on_focus_out)
         main_win.connect('focus-in-event', self.on_focus_in)
         main_win.connect('size-allocate', self.on_resize)
-        
-        
+
+
     def set_images(self, cover_path, thumb_path):
         self.cover_window.set_image(cover_path)
         self.thumb_window.set_image(thumb_path)
-        if self.show_thumb is None:
-            self.onCoverClicked(None, None, True)
-            # This somehow fixes the compiz problem where the cover was only 
-            # shown after the window had been redrawn
-            self.thumb_window.update_position()
-        elif self.has_focus:
+
+        # This somehow fixes the compiz problem where the cover was only
+        # shown after the window had been redrawn
+        #self.thumb_window.update_position()
+        #self.thumb_window.show()
+        #self.cover_window.update_position()
+        #self.cover_window.show()
+
+        if self.has_focus:
             self.onCoverClicked(None, None, self.show_thumb)
-        
-        
+
+
     def onCoverClicked(self, widget, event, show_thumb):
         """ Destroy the cover window """
         if show_thumb:
-            self.cover_window.hide()
-            self.thumb_window.update_position()
             self.thumb_window.show()
+            self.thumb_window.update_position()
+            self.cover_window.hide()
         else:
-            self.cover_window.update_position()
             self.cover_window.show()
+            self.cover_window.update_position()
             self.thumb_window.hide()
         self.show_thumb = show_thumb
-        
-        
+
+
     def on_focus_out(self, widget, event):
         self.has_focus = False
         self.cover_window.hide()
         self.thumb_window.hide()
-        
-        
+
+
     def on_focus_in(self, widget, event):
         self.has_focus = True
         self.onCoverClicked(None, None, self.show_thumb)
-        
-    
+
+
     def on_resize(self, win, rectangle):
         self.cover_window.update_position()
         self.thumb_window.update_position()
-        
-            
-            
+
+
+
 class CoverWindow(gtk.Window):
     def __init__(self):
         gtk.Window.__init__(self, gtk.WINDOW_POPUP)
@@ -173,10 +182,10 @@ class CoverWindow(gtk.Window):
         frame.set_shadow_type(gtk.SHADOW_IN)
         frame.add(self.evtBox)
         self.add(frame)
-        
+
         self.has_cover = False
-        
-        
+
+
     def set_image(self, path):
         """
         Change the current image to imgPath.
@@ -191,15 +200,15 @@ class CoverWindow(gtk.Window):
             self.image.set_from_file(path)
             self.update_position()
             self.has_cover = True
-        
-        
+
+
     def update_position(self):
         # Make the window exactly as big as the image, not bigger
         self.resize(2, 2)
-        
+
         # Use default when tree is not yet realized
         x, y = 0,0
-        
+
         # Position the window in the bottom right corner
         wTree = tools.prefs.getWidgetsTree()
         scrolled = wTree.get_widget('scrolled-tracklist')
@@ -208,26 +217,26 @@ class CoverWindow(gtk.Window):
             tree_x, tree_y, tree_width, tree_height = tree.get_allocation()
             # Get the window, the tracktree is painted on
             tree_win = tree.get_parent_window()
-            
+
             # Get absolute position of the gtk.gdk.Window without the window decos
             orig_tree_win_x, orig_tree_win_y = tree_win.get_origin()
-            
+
             # Calculate absolute position of upper left corner
             x = orig_tree_win_x + tree_x
             y = orig_tree_win_y + tree_y
-            
+
             # get lower right corner
             x, y = x + tree_width, y + tree_height
-            
+
             # leave a little padding space
             x, y = x-10, y-9
-        
+
         pixbuf = self.image.get_pixbuf()
         width  = pixbuf.get_width()
         height = pixbuf.get_height()
         self.move(int(x - width), int(y - height))
-            
-        
+
+
     def show(self):
         if self.has_cover:
             gtk.Window.show_all(self)
