f899964b9e25f563a17b9879a9f67f47db8bc7c4
Jendrik Seipp <jendrikseipp@web.de>
2011-02-07 12:14:48 +0100

    copyright in pogo.py

--- pogo.py
+++ pogo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Authors: Ingelrest François (Francois.Ingelrest@gmail.com)
-#          Jendrik Seipp (jendrikseipp@web.de)
+# Copyright (c) 2007  François Ingelrest (Francois.Ingelrest@gmail.com)
+# Copyright (c) 2010  Jendrik Seipp (jendrikseipp@web.de)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
