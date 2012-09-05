994e86e9d0ea154690bb9979e51de4a192f5c6e0
Jendrik Seipp <jendrikseipp@web.de>
2012-09-05 16:10:36 +0200

    bump version to 0.8

--- src/tools/consts.py
+++ src/tools/consts.py
@@ -30,7 +30,7 @@ socketTimeout = 10
 
 # --- Strings
 appName      = 'Pogo'
-appVersion   = '0.7'
+appVersion   = '0.8'
 appNameShort = 'pogo'
 commands = ['play', 'pause', 'prev', 'next', 'stop']
 
