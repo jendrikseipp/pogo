329f8f6283073a10b0619642baf8dde029f1cae0
Jendrik Seipp <jendrikseipp@web.de>
2013-01-19 12:30:03 +0100

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,6 +5,8 @@ set -e
 # if credentials are missing run:
 # manage-credentials create -c ubuntu-dev-tools -l 2
 
+NEWS="$1"
+
 VERSION=`python -c "import sys; sys.path.append('src/tools'); import consts; print consts.appVersion"`
-./doc/create-releasenotes.py
-lp-project-upload pogo $VERSION "pogo-$VERSION.tar.gz" $VERSION /dev/null RELEASENOTES
+# Tool resides in package lptools.
+lp-project-upload pogo $VERSION "pogo-$VERSION.tar.gz" $VERSION /dev/null "$NEWS"
