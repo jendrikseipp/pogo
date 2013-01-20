1e89de3b62d9d1ff59df32c142e3bf7ad267d946
Jendrik Seipp <jendrikseipp@web.de>
2013-01-20 22:44:15 +0100

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 set -e
+set -u
 
 # if credentials are missing run:
 # manage-credentials create -c ubuntu-dev-tools -l 2
 
 NEWS="$1"
+
+cd "$(dirname "$0")"
 
 VERSION=`python -c "import sys; sys.path.append('src/tools'); import consts; print consts.appVersion"`
 # Tool resides in package lptools.
